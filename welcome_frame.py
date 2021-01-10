fcb5735433c2d4a49677ecca17edbb5bcc69e5b8
OrrMatzkin <orr.matzkin@gmail.com>
2021-01-10 17:45:01 +0200

    changed the subtitle to be packed instead of placed

--- welcome_frame.py
+++ welcome_frame.py
@@ -8,7 +8,7 @@ class WelcomeFrame(tk.Frame):
     WelcomeFrame is an inherited object from tkinter Frame class.
     """
     _TITLE = 'Boggle !'
-    _SUB_TITLE = '3-Minute Word Game'
+    _SUB_TITLE = '3-Minute Word Game            '
     _COPYRIGHTS = '@ Avihu Almog & Orr Matzkin'
     _SUB_TITLE_FONT = 'Hobo Std'
     _FONT_TITLE = 'Ubicada Pro'
@@ -28,15 +28,15 @@ class WelcomeFrame(tk.Frame):
                               font=(self._FONT_TITLE, 84),
                               fg="red3").pack(side='top', expand='yes')
 
+        self.sub_title = tk.Label(self, text=self._SUB_TITLE,
+                                  font=(self._SUB_TITLE_FONT, 14),
+                                  fg="grey").pack(side='top', expand='no')
+
         load = Image.open("logo_Boggle.png")
         render = ImageTk.PhotoImage(load)
         self.game_logo = tk.Label(self, image=render)
         self.game_logo.image = render
         self.game_logo.pack(side='top', expand='no')
-
-        self.sub_title = tk.Label(self, text=self._SUB_TITLE,
-                                  font=(self._SUB_TITLE_FONT, 14),
-                                  fg="grey").place(relx=0.37, rely=0.242)
 
         self.play_button = tk.Button(self, text='Play', font=(self._FONT, 18),
                                      width=16, command=lambda: controller.
@@ -61,3 +61,4 @@ class WelcomeFrame(tk.Frame):
         self.huji_logo = tk.Label(self, image=render)
         self.huji_logo.image = render
         self.huji_logo.place(relx=0.72, rely=0.85)
+        # self.huji_logo.pack(side='right', anchor='se', expand='yes')
